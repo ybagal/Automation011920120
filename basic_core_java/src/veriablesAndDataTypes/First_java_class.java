3a0ca4e4cf9fc99e1ebd1882b525945e19283db0
Unknown <unknown@example.com>
2020-01-25 12:47:56 +0530

    cummiting_first_java_class

---
+++ basic_core_java/src/veriablesAndDataTypes/First_java_class.java
@@ -0,0 +1,11 @@
+package veriablesAndDataTypes;
+
+public class First_java_class {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the world of automation");
+		
+		
+	}
+}
